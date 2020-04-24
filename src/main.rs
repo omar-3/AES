b16c0b8e885b20a146a216265a02ff684e208fe2
omar-3 <s-omarkhaled@zewailcity.edu.eg>
2020-04-24 04:50:14 +0200

    just spent 10 hours writing 3 macros for tables :D

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#![allow(non_snake_case)]
+
+use AES::{g8mult, sbox, invsbox};
+
 fn main() {
-    println!("it is the first time for me to write rust :(");
+    let khaled = g8mult!(57, 13);
+    println!("{}", khaled);
+
+    let mut row1MixColumn: Vec<u8> = Vec::new();
+    row1MixColumn.push(0x02);
+    row1MixColumn.push(0x03);
+    row1MixColumn.push(0x01);
+    row1MixColumn.push(0x01);
+    println!("{}", row1MixColumn.len());
+    println!("{:?}", row1MixColumn);
+    let mut column1InvMixColumn: Vec<u8> = Vec::new();
+    column1InvMixColumn.push(0x0e);
+    column1InvMixColumn.push(0x09);
+    column1InvMixColumn.push(0x0d);
+    column1InvMixColumn.push(0x0b);
+    println!("{}", column1InvMixColumn.len());
+    println!("{:?}", column1InvMixColumn);
+    let mut z : Vec<u8> = Vec::with_capacity(4);
+    for ((zval, aval), bval) in z.iter_mut().zip(&row1MixColumn).zip(&column1InvMixColumn) {
+        *zval = *aval + *bval;
+    }
+    
+    println!("{:?}", z);
+    let omar = sbox!(0xc2);
+    println!("{}", omar);
+    let shh = invsbox!(10);
+    println!("{}", shh);
 }
